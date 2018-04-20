34888ae4b9a0da15943f44108db5f991c791f544
coder-dude <adwaitthattey@gmail.com>
2018-04-20 23:35:38 +0530

    "Now a random time-table can be generated"

--- Genetic/genetic_classes.py
+++ Genetic/genetic_classes.py
@@ -9,27 +9,34 @@ class details :
         course_iterator = utilities.get_all_courses()
         I = next(course_iterator)
         while 1:
-            L = list()
-            L.append(I.professor)
-            L.append(I.no_of_classes_per_week)
-            self.course_details[I.id] = list(L)
+            D = dict()
+            D["professor"] = I.professor
+            D["no_of_classes_per_week"] = I.no_of_classes_per_week
+            D["time_slots_required"] = int(float(I.time_duration)*2) # As time-duration is in hours but each slot is of 0.5 hour
+            # time_slots is the time slots required for each class
+            self.course_details[I.id] = D
             try:
                 I = next(course_iterator)
             except StopIteration :
                 break
 
-        print(self.course_details)            
+    def disp_all_details(self) :
+        print("Course Details :")
+        for i in self.course_details.keys() :
+            print(i , end = " : ")
+            print(self.course_details[i])
+        
 
 
 class chromosome :
     ''' A class that contains 1 chromosome for genetic algorithms '''
     def __init__(self , main_data) :
-        self.timeline = [None]*10*6
-        self.break_points = [4, 14, 24, 34, 44, 54]
+        self.timeline = [None]*20*6 # Each time-slot is of half hour
+        self.break_points = [8,9,28,29,48,49,68,69,88,89,108,109]
         self.fitness_list = [0]*2
         self.fitness = sum(self.fitness_list)
         self.course_times = dict()
-        self.number_of_vacant_slots = 0
+        self.vacant_slots = list()
 
 
         for i in range(len(self.timeline)) :
@@ -39,18 +46,26 @@ class chromosome :
         
         for i in main_data.course_details.keys() :
             self.course_times[i] = list()
-            no_of_points_to_be_generated = main_data.course_details[i][1]
-            print(no_of_points_to_be_generated)
+            no_of_points_to_be_generated = main_data.course_details[i]["no_of_classes_per_week"]
+            # print(no_of_points_to_be_generated)
             import random
             while(no_of_points_to_be_generated > 0) :
                 N = random.randint(0,len(self.timeline) - 1)
                 if(N in self.break_points) : continue #break time. generate again.
-                if N in self.course_times[i] : continue # slot taken by same course. Generate again    
+                elif N in self.course_times[i] : continue # slot taken by same course. Generate again    
                 else :
-                    self.course_times[i].append(N)
-                    no_of_points_to_be_generated-=1    
+                    templist = list()
+                    for k in range(main_data.course_details[i]["time_slots_required"]) :
+                        if N+k in self.break_points or N+k >= len(self.timeline) : #point must not be in breaks and should not be out of index
+                            break
+                        elif k>0 and (N+k)%(len(self.timeline)/6)==0 : break # New Day has started. Cant schedule.
+                        templist.append(N+k)
 
-        print(self.course_times);
+                    else :
+                        self.course_times[i].extend(templist)
+                        no_of_points_to_be_generated-=1    
+
+        print(self.course_times)
 
         for i in self.course_times.keys() :
             for j in self.course_times[i] :
@@ -59,8 +74,8 @@ class chromosome :
                     self.timeline[j] = list()
                     self.timeline[j].append(i)
 
-        for i in self.timeline :
-            if i==None : self.number_of_vacant_slots+=1 
+        for i in range(len(self.timeline)) :
+            if self.timeline[i]==None : self.vacant_slots.append(i) 
 
 
     def display_course_times(self) :
@@ -69,16 +84,30 @@ class chromosome :
             print(i , end=" ")
             print(self.course_times[i])
         print("----------")    
-    def display_timeline(self) :
+    def display_timeline(self , name="timeline") :
+        # TimeLine is always written to a CSV File for easier analysis
+        F = open(name+".csv" , mode="w")
+        S = ""
         for i in range(len(self.timeline)) :
-            if(i%10==0) : print()
-            print(self.timeline[i] , end="  ")
+            Stemp=""    
+            if(self.timeline[i]=="Break") : Stemp = "Break"
+            elif(self.timeline[i]!=None) :
+                for j in self.timeline[i] :
+                    Stemp+= j + "+"
+
+            if(i != 0 and i % 19 == 0):
+                F.write(S + "\n")
+                S = ""
+            
+            S+= Stemp + ","
+
+        F.close()        
 
     def display_details(self) :
         print("------\n Details\n")
         print("break points : " , self.break_points)
         print("fitness : " , self.fitness)
-        print("no of vacant slots :" , self.number_of_vacant_slots)
+        print("no of vacant slots :" , self.vacant_slots)
         print("------")
 
 
@@ -104,7 +133,8 @@ class fitness_functions :
 
 
 C = details()
+C.disp_all_details()
 Chr = chromosome(C)
 Chr.display_details()
 Chr.display_course_times()
-Chr.display_timeline()
+Chr.display_timeline("timeline1")
