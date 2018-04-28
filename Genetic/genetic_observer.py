60376a962ed992666b33efc38e0f6bbd00427e1d
coder-dude <coderdude1999@gmail.com>
2018-04-29 00:37:56 +0530

    Observer crossover completed

--- Genetic/genetic_observer.py
+++ Genetic/genetic_observer.py
@@ -14,9 +14,12 @@ class observer :
             self.parents.append(chrmo)
         self.parents.sort(key = lambda x: x.fitness , reverse=True)
 
-    def print_parents(self) :
+        self.print_fitness()
+
+    def print_fitness(self , chrms = None) :
+        if chrms==None : chrms = self.parents
         print()
-        for i in self.parents :
+        for i in chrms :
             print(i.fitness , end=" ")
 
         print()            
@@ -26,19 +29,70 @@ class observer :
         children.extend(self.g_algo.crossover(self.parents[0] , self.parents[1]))
         children.extend(self.g_algo.crossover(self.parents[0] , self.parents[2]))
         children.extend(self.g_algo.crossover(self.parents[2] , self.parents[1]))
-        # children.extend(self.g_algo.crossover(self.parents[i] , self.parents[self.number_of_parents//2 + i]))
+        
+        for i in range(self.number_of_parents//2) :
+            children.extend(self.g_algo.crossover(self.parents[i] , self.parents[self.number_of_parents//2 + i]))
+
+        if self.number_of_parents%2 : 
+            children.extend(self.g_algo.crossover(self.parents[self.number_of_parents//2] , self.parents[self.number_of_parents - 1]))           
+
+        try :
+            for i in range(4) :
+                children.extend(self.g_algo.crossover(self.parents[self.number_of_parents//2 + i] , self.parents[self.number_of_parents//2 - i]))
+        except IndexError : pass
+
+        # children.extend(self.g_algo.crossover(self.parents[self.number_of_parents-3] , self.parents[self.number_of_parents-2]))
+        # children.extend(self.g_algo.crossover(self.parents[self.number_of_parents-3] , self.parents[self.number_of_parents-1]))
+        # children.extend(self.g_algo.crossover(self.parents[self.number_of_parents-1] , self.parents[self.number_of_parents-2]))
+
+        self.print_fitness(children)
+        
+        children.sort(key = lambda x : x.fitness , reverse=True)
+
+        self.print_fitness(children)
+
+        self.parents = list(self.select_best_chromosomes(self.parents , children , self.number_of_parents))
+
+        del(children)
+
+        self.print_fitness()
 
 
     def select_best_chromosomes(self , chl1 , chl2 , number=None) :
         if number==None : number = self.number_of_parents
-
+        elif number > len(chl1) + len(chl2) :
+            print("EXPECTED NUMBER OF BEST CHILDREN IS MORE THAN SUM OF BOTH PARENTS! \n TERMINATING...")
+            exit() 
         i=j=0
-        while number>0 :
+        while number>0:
             number-=1
-            if chl1[i].fitness >= chl2.fitness[j] :
+            try :
+                if chl1[i].fitness >= chl2[j].fitness :
+                    yield chl1[i]
+                    i+=1
+
+                else :
+                    yield chl2[j]
+                    j+=1
+            except IndexError :
+                print("i or j exhausted!!")
+                break
+
+        if i > len(chl1) :
+            while number>0 :
+                yield chl2[j]
+                j+=1
+                number-=1 
+
+        elif j > len(chl2) :
+            while number>0 :
                 yield chl1[i]
                 i+=1
+                number-=1
 
-            else :
-                yield chl2[j]
-                j+=1    
+                              
+
+
+if __name__=="__main__" :
+    obs = observer()
+    obs.perform_crossover()
