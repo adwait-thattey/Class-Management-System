4ae66466c7c2f2bfa35d0af0dfdcc87aa79d286c
Aaquib Niaz <aaquibniaz3600@gmail.com>
2018-04-29 20:47:11 +0530

    app.py configured a bit

--- flask/app.py
+++ flask/app.py
@@ -2,17 +2,16 @@ from flask import Flask ,render_template,request,session,redirect,url_for
 #import the functions
 app = Flask(__name__)
 
-@app.route('/send',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def send():
     if request.method == 'POST':
-        age = request.form['age']
-        name = request.form[]
+        email = request.form['email']
+        #name = request.form[]
+        password = request.form['password']
 
-
-        return render_template('age.html',age=age)
+        return render_template('webpage.html')
         
     return render_template('form.html')
-    
 
 if __name__ == "__main__":
     app.run(debug=True)
